3fbce9a9c58e89849b41d326bc4b900bc162b85d
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-07-25 12:53:25 +0300

    fix03

--- build/utils/decodeJwt.js
+++ build/utils/decodeJwt.js
@@ -4,12 +4,7 @@ exports.decodeJwtResponse = void 0;
 const decodeJwtResponse = (token) => {
     const base64Url = token.split(".")[1];
     const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    const jsonPayload = decodeURIComponent(atob(base64)
-        .split("")
-        .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-    })
-        .join(""));
+    const jsonPayload = Buffer.from(base64, "base64").toString("utf-8");
     return JSON.parse(jsonPayload);
 };
 exports.decodeJwtResponse = decodeJwtResponse;
--- src/utils/decodeJwt.ts
+++ src/utils/decodeJwt.ts
@@ -1,14 +1,6 @@
-export const decodeJwtResponse = (token: any) => {
+export const decodeJwtResponse = (token: string) => {
   const base64Url = token.split(".")[1];
   const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  const jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split("")
-      .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join("")
-  );
-
+  const jsonPayload = Buffer.from(base64, "base64").toString("utf-8");
   return JSON.parse(jsonPayload);
-}
+};
